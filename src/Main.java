e0b9c8224b2bc9393e7393a8506a9715f8b6ecac
Tahmina2005 <thia4939@gmail.com>
2022-05-25 23:22:38 -0930

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,29 @@
+public class Main {
+
+    public static void main(String[] args) {
+        String nameDog;
+
+        final int NUM = 10;
+
+        String word = "250";
+        nameDog = NUM + word;
+
+        System.out.println(nameDog);
+
+        if (NUM < 0){
+            System.out.println("number < 0");
+        }
+        else if (NUM > 0) {
+            System.out.println("number > 0");
+        }
+        else{
+            System.out.println("0");
+        }
+        Console.Write("George");
+        String name = Console.Readline() ;
+        Console.WriteLine("Hello!");
+
+
+    }
+}
+
